88e4caf409a805a9ab88e4b35f3b726465128c99
Thiago Sandi <thiago.sandi91@gmail.com>
2019-06-14 02:22:47 -0300

    adding animation to the list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -13,8 +15,10 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({opacity:0}), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))]), {optional: true}),
+
+          query(':leave', stagger('300ms', [
+            animate('.6s ease-in', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+              style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+            ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number;
   btnText: string = "Add an Item";
   goalText: string = "My First life goal";
-  goals: string[] = [];
+  goals: string[] = ['Go to Iceland', 'Go to USA', 'Meditate every day'];
 
   constructor() { }
 
@@ -21,6 +44,11 @@ export class HomeComponent implements OnInit {
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
+    this.itemCount = this.goals.length
+  }
+
+  removeItem(item){
+    this.goals.splice(item, 1);
     this.itemCount = this.goals.length
   }
 
